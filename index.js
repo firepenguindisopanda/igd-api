45c7b420ecd02ce51cb076b26a54c16a08086137
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-08 22:41:28 -0400

    Updated: added config to setup tables in db

--- index.js
+++ index.js
@@ -5,11 +5,34 @@ const { errorHandler } = require('./middleware/errorHandler');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
+const db = require('./models');
+const Role = db.role;
 
 
 const app = express();
 const PORT = process.env.PORT || 3500;
 
+db.sequelize.sync({ force: true }).then(() => {
+    console.log('Drop and Resync Db');
+    initialiseRoles();
+});
+
+const initialiseRoles = () => {
+    Role.create({
+        id: 1,
+        name: 'user'
+    });
+
+    Role.create({
+        id: 2,
+        name: 'moderator'
+    });
+
+    Role.create({
+        id: 3,
+        name: 'admin'
+    });
+};
 
 app.use(logger);
 app.use(cors(corsOptions));
