cf774eed129db166d0e0769fe3a074af5400ac60
firepenguindisopanda <nicosmith.smith3@gmail.com>
2022-12-17 19:13:22 -0400

    Added: a practice function to test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -13,3 +13,20 @@ exports.adminBoard = (req, res) => {
 exports.moderatorBoard = (req, res) => {
     res.status(200).send("Moderator Content.");
 };
+
+exports.knapsackProblem = (capacity, items = []) => {
+    const matrix = [];
+    for (let i = 0; i < items.length; i++) {
+        matrix[i] = [];
+        for (let j = 0; j <= capacity; j++) {
+            if (i === 0) {
+                matrix[i][j] = 0;
+            } else if (items[i].weight > j) {
+                matrix[i][j] = matrix[i - 1][j];
+            } else {
+                matrix[i][j] = Math.max(matrix[i - 1][j], matrix[i - 1][j - items[i].weight] + items[i].value);
+            }
+        }
+    }
+    return matrix;
+}
